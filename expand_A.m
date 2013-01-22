0eee24eddfa51d4d11cd7710eb7188cccdc8a338
David Love <dlove@math.arizona.edu>
2013-01-22 14:23:17 -0700

    Updating expand_A.m to November 8, 2012 version of LP_Optimization.m

--- expand_A.m
+++ expand_A.m
@@ -1,20 +1,23 @@
-function A_full = expand_A(A,Ap,Period)
+function A_full = expand_A(A,A_st,A_lag,Period,Time_Lag)
 
-xc = size(A,1);
+xc = size(A,1); % Initial Matrix without storage carry over
 xd = size(A,2);
-xg = size(Ap,1);
-xh = size(Ap,2);
-
-if Period > 1 % condition for solution over 1 year period
-    A_full(1:xg,1:xh)=Ap;
+% xg = size(A_st,1); % Initial Matrix with storage carry over (incorporates two time periods)
+% xh = size(A_st,2);
+A_full = zeros(xc*Period,xd*Period);
+A_full(1:xc,1:xd)=A;
+A_lag_ini = A_st + A_lag;
+if Period > 1 % condition for more than one time step
     for xe = 2:Period
         xf = xe - 1;
-        if xe == Period
-            A_full(xf*xc+1:xe*xc,xf*xd+1:xe*xd) = A;
+        A_full(xf*xc+1:xe*xc,xf*xd+1:xe*xd)=A;
+        if Period/Time_Lag == Period
+            A_full(xf*xc+1:xe*xc,(xf-1)*xd+1:xf*xd)=A_lag_ini;
+        elseif xf >= Time_Lag
+            A_full(xf*xc+1:xe*xc,(xf-Time_Lag)*xd+1:(xf-Time_Lag+1)*xd)=A_lag;
+            A_full(xf*xc+1:xe*xc,(xf-1)*xd+1:xf*xd)=A_st;
         else
-            A_full(xf*xc+1:xf*xc+xg,xf*xd+1:xe*xd) = Ap;
+            A_full(xf*xc+1:xe*xc,(xf-1)*xd+1:xf*xd)=A_st;
         end
-        
     end
-else A_full = A;
 end
