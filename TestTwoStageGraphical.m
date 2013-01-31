9c17f6885c4db183f4a3c2e1afdd042e74950e51
David Love <dlove@math.arizona.edu>
2013-01-31 13:59:21 -0700

    Added assertions to TestTwoStage

--- TestTwoStageGraphical.m
+++ TestTwoStageGraphical.m
@@ -4,6 +4,7 @@ simpleLP = InitializeSimpleTwoStageLP();
 
 gp = 0.5;
 obs = ceil(50*rand(1,4));
+obs = [35  28  22  33];
 
 lrlp = LRLP( simpleLP, gp, obs );
 
@@ -12,12 +13,39 @@ while lrlp.currentObjectiveTolerance > lrlp.objectiveTolerance
     if exitFlag ~= 1
         disp(['exitFlag = ' num2str(exitFlag)])
     end
+    cS = lrlp.candidateSolution;
+    
     lrlp.SolveSubProblems;
+    assert( isequal( cS, lrlp.candidateSolution ), ...
+        num2str([cS, lrlp.candidateSolution]) )
+    
     lrlp.GenerateCuts;
-%     lrlp.UpdateTrustRegionSize;
-    lrlp.UpdateSolutions;
-    lrlp.UpdateTolerances;
-    lrlp.WriteProgress;
+    assert( ~lrlp.candidateMuIsFeasible ...
+        || isequal( cS, lrlp.candidateSolution ), ...
+        num2str([cS, lrlp.candidateSolution]) )
+    cS = lrlp.candidateSolution;
+    
+    % Note, putting plot after updating trust region or solution will
+    % result in it plotting the wrong trust region
 %     lrlp.Plot;
-    pause(0.5)
+%     assert( isequal( cS, lrlp.candidateSolution ), ...
+%         num2str([cS, lrlp.candidateSolution]) )
+    
+%     lrlp.UpdateTrustRegionSize;
+%     assert( isequal( cS, lrlp.candidateSolution ), ...
+%         num2str([cS, lrlp.candidateSolution]) )
+    
+    lrlp.UpdateSolutions;
+    assert( isequal( cS, lrlp.candidateSolution ), ...
+        num2str([cS, lrlp.candidateSolution]) )
+    
+    lrlp.UpdateTolerances;
+    assert( isequal( cS, lrlp.candidateSolution ), ...
+        num2str([cS, lrlp.candidateSolution]) )
+    
+    lrlp.WriteProgress;
+    assert( isequal( cS, lrlp.candidateSolution ), ...
+        num2str([cS, lrlp.candidateSolution]) )
+    
+%     pause(.5)
 end
