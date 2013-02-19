f45e235154eb2640e3f672a30f617397480ff721
David Love <dlove@math.arizona.edu>
2013-02-19 15:59:47 -0700

    TestOneStageMatch.m uses all 16 scenarios in 'SP C/'
    and reports which scenario resulted in an error

--- TestOneStageMatch.m
+++ TestOneStageMatch.m
@@ -1,17 +1,20 @@
 function TestOneStageMatch
 
-dirs = 1:12;
+dirs = 1:16;
 Period = 41;
 timeLag = 1;
 
 for ii=1:length(dirs)
-    inputLocation = strcat('all_scenarios/',num2str(dirs(ii)),'/');
+    inputLocation = strcat('SP C/',num2str(dirs(ii)),'/');
     clear lp;
     lp = LPModel(inputLocation,Period,timeLag);
-    [Q fval] = linprog( lp.c, [], [], lp.A, lp.b, lp.l, lp.u );
+    [Q,fval] = linprog( lp.c, [], [], lp.A, lp.b, lp.l, lp.u );
 %     Q = roundn(Q',-2);
     Q = lp.ReadResults(Q,inputLocation);
     
+    disp(' ')
+    disp(['Checking folder: ' inputLocation])
+    disp(' ')
     alicia = load([inputLocation,'orig_variables.mat']);
     assert(nnz(lp.A ~= alicia.A_full) == 0)
     assertEqual(lp.b,alicia.b_vec)
@@ -21,5 +24,8 @@ for ii=1:length(dirs)
     assertEqual(Q,alicia.Q)
     assertEqual(fval,alicia.fval)
     assertEqual(lp.Final,alicia.Final)
+    
+    disp(['Folder ' inputLocation ' passed tests'])
+    disp(' ')
 end
 
