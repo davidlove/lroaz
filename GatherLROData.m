755d33512ffcb1dc015792d357470f4ae0ddeca2
David Love <dlove@math.arizona.edu>
2013-06-25 12:26:26 -0700

    Update GatherLROData.m to work with PhiDivergence class

--- GatherLROData.m
+++ GatherLROData.m
@@ -1,4 +1,4 @@
-function GatherLROData(saveFileName )
+function GatherLROData( saveFileName )
 
 if nargin < 1
     saveFileName = 'saved_variables.mat';
@@ -17,19 +17,20 @@ timeLag = 1;
 firstStageYears = 5;
 
 lpModel = LPModel( waterFolders, years, timeLag, firstStageYears );
+phi = PhiDivergence('lro');
 
 obs = 1*[1 1 1 1];
 
 % dgp = 0.01;
 % gp = dgp:dgp:1-dgp;
-gp = 10.^linspace(-3,0,100);
-gp(end) = mean(gp(end-1:end));
+rho = 10.^linspace(-3,0,100);
+rho(end) = mean(rho(end-1:end));
 
-iiSet = 1:length(gp);
+iiSet = 1:length(rho);
 
 for ii = iiSet
     timeStart = tic;
-    [solvedLRLP,c1,n1] = SolveLRLP( lpModel, gp(ii), obs, 'multi' );
+    [solvedLRLP,c1,n1] = SolveLRLP( lpModel, phi, obs, rho(ii), 'multi' );
     timeIndiv = toc(timeStart);
     
     p1 = solvedLRLP.pWorst;
@@ -37,21 +38,21 @@ for ii = iiSet
     m1 = solvedLRLP.bestSolution.Mu;
     l1 = solvedLRLP.bestSolution.Lambda;
     s1 = solvedLRLP.bestSolution.SecondStageValues;
-    r1 = solvedLRLP.relativeLikelihood;
+    r1 = solvedLRLP.calculatedDivergence;
     z1 = solvedLRLP.ObjectiveValue;
     
     if ii==1
-        pWorst = zeros(length(p1),length(gp));
-        x = zeros(length(x1),length(gp));
-        lambda = zeros(length(l1),length(gp));
-        mu = zeros(length(m1),length(gp));
-        scenCosts = zeros(length(s1),length(gp));
-        objVals = zeros(length(z1),length(gp));
-        likelihood = zeros(length(r1),length(gp));
+        pWorst = zeros(length(p1),length(rho));
+        x = zeros(length(x1),length(rho));
+        lambda = zeros(length(l1),length(rho));
+        mu = zeros(length(m1),length(rho));
+        scenCosts = zeros(length(s1),length(rho));
+        objVals = zeros(length(z1),length(rho));
+        calcRho = zeros(length(r1),length(rho));
 %         exitFlags = zeros(1,length(gp));
-        numProbs = zeros(1,length(gp));
-        numCuts = zeros(1,length(gp));
-        timeRuns = zeros(1,length(gp));
+        numProbs = zeros(1,length(rho));
+        numCuts = zeros(1,length(rho));
+        timeRuns = zeros(1,length(rho));
     end
     
     pWorst(:,ii) = p1';
@@ -60,15 +61,15 @@ for ii = iiSet
     mu(:,ii) = m1;
     scenCosts(:,ii) =s1;
     objVals(:,ii) = z1;
-    likelihood(:,ii) = r1;
+    calcRho(:,ii) = r1;
 %     exitFlags(ii) = eF;
     numProbs(ii) = n1;
     numCuts(ii) = c1;
     timeRuns(ii) = timeIndiv;
     
-    save(saveFileName,'gp','pWorst','x','lambda','mu', ...
+    save(saveFileName,'rho','pWorst','x','lambda','mu', ...
         'scenCosts','objVals', ...
-        'likelihood', ...
+        'calcRho', ...
         'numProbs','numCuts','timeRuns');
 %         'exitFlags')
 
