556176cf9243c316134edace61b1166d731e6ee5
David Love <dlove@math.arizona.edu>
2013-07-28 15:26:33 -0700

    Add function ExtensiveForm to generate and solve SLP-2
    	new file:   ExtensiveForm.m

---
+++ ExtensiveForm.m
@@ -0,0 +1,29 @@
+function [x,c,A,b,l,u] = ExtensiveForm(lp)
+
+c = lp.c;
+b = lp.b;
+l = lp.l;
+u = lp.u;
+[i,j,v] = find(lp.A);
+prob = 1./lp.numScenarios;
+
+for ss=1:lp.numScenarios
+    c = [c, prob*lp.Getq(ss)'];
+    b = [b; lp.Getd(ss)];
+    l = [l; lp.Getl2(ss)];
+    u = [u; lp.Getu2(ss)];
+    
+    rows = max(i);
+    cols = max(j);
+    [i2,j2,v2] = find(-lp.GetB(ss));
+    i = [i; i2+rows];
+    j = [j; j2];
+    v = [v; v2];
+    [i3,j3,v3] = find(lp.GetD(ss));
+    i = [i; i3+rows];
+    j = [j; j3+cols];
+    v = [v; v3];
+end
+A = sparse(i,j,v);
+
+x = linprog(c,[],[],A,b,l,u);
