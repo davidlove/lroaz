56767904f6cd378c3cd05fec45dcaabab449639c
David Love <dlove@math.arizona.edu>
2013-09-09 13:30:21 -0700

    Add indication of accurate prediction

--- PlotValueOfData.m
+++ PlotValueOfData.m
@@ -2,6 +2,8 @@ function PlotValueOfData( rho, actualRelative, predicted )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+assert(nnz(predicted) == nnz(predicted==1))
+
 colors = {'b','g','r','c','m','y'};
 actLine = ':';
 predLine = '-';
@@ -12,7 +14,7 @@ figure(1)
 clf
 hold on
 for ii=1:n
-    posVals = predicted(ii,:) > 0;
+    posVals = predicted(ii,:) == 1;
     plot(rho(posVals), 100*actualRelative(ii,posVals), strcat(colors{ii},predLine), ...
         'LineWidth',3)
 end
@@ -21,12 +23,17 @@ yl(1) = min(0,yl(1));
 
 for ii=1:n
     plot(rho, 100*actualRelative(ii,:), strcat(colors{ii},actLine), ...
-        'LineWidth',4, 'MarkerSize',12)
+        'LineWidth',3 )
+    % Go over predicted values again to cover actual in that space
+    posVals = predicted(ii,:) == 1;
+    plot(rho(posVals), 100*actualRelative(ii,posVals), strcat(colors{ii},predLine), ...
+        'LineWidth',3)
 end
+
 ylim(yl)
 
 xlabel('\rho', 'FontSize',14)
-ylabel('Relative Decrease From Observation (%)', 'FontSize',14)
+ylabel('Cost Decrease (%)', 'FontSize',14)
 
 hold off
 
