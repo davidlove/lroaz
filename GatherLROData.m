3f6e2ecff5bc0ea827136e5a7e8f0184502a101f
David Love <dlove@math.arizona.edu>
2013-09-13 14:09:04 -0700

    Update GatherLROData to use keyword arguments

--- GatherLROData.m
+++ GatherLROData.m
@@ -1,8 +1,42 @@
-function GatherLROData( phiType, lpModel, saveFileName )
+function GatherLROData( varargin )
+% GatherLROData solves the phi-divergence model for multiple values of rho
+% and reports the results
+%
+% Keyword arguments:
+%    lp: LPModel object defining the SLP-2
+%    obs: vector of observations numbers of the scenarios
+%    phi: PhiDivergence object or string defining the phi-divergence
+%    savefile: String or file object denoting the file to save data to
 
-if nargin < 3
-    saveFileName = 'saved_variables.mat';
-    if nargin < 2
+if mod(length(varargin),2) == 1
+    error('Arguments must be key, value pairs')
+end
+
+for vv = 1:2:length(varargin)
+    key = varargin{vv};
+    value = varargin{vv+1};
+    switch key
+        case 'lp'
+            lp = value;
+        case 'obs'
+            obs = value;
+        case 'phi'
+            if isa(value, 'PhiDivergence')
+                phi = value;
+            elseif ischar(value)
+                phi = PhiDivergence( value );
+            else
+                error('Phi must be PhiDivergence object or string')
+            end
+        case 'savefile'
+            saveFileName = value;
+        otherwise
+            error(['Unknown variable ', key])
+    end
+end
+
+% Default variable assignments
+if ~exist('lp', 'var')
         waterFolders = { ...
             'SP C/5/', ...
             'SP C/6/', ...
@@ -13,16 +47,20 @@ if nargin < 3
         timeLag = 1;
         firstStageYears = 5;
         
-        lpModel = LPModel( waterFolders, years, timeLag, firstStageYears );
-        if nargin < 1
-            phiType = 'kl';
-        end
-    end
+        lp = LPModel( waterFolders, years, timeLag, firstStageYears );
+end
+if ~exist('obs', 'var')
+    obs = ones(1,lp.numScenarios);
+end
+if ~exist('savefile', 'var')
+    saveFileName = 'saved_variables.mat';
 end
 
-phi = PhiDivergence( phiType );
+if ~exist('phi', 'var')
+    error('Must specify a phi-divergence type')
+end
 
-obs = ones(1,lpModel.numScenarios);
+% -------------------------------------------------------------------
 
 if isinf(phi.limit) && any(obs == 0)
     error(['Phi divergence ' phi.divergence ' does not allow poping up probabilities'])
@@ -31,13 +69,13 @@ end
 alpha = 10.^linspace(-2.93,0,50);
 alpha(end) = mean(alpha(end-1:end));
 rho = phi.SecondDerivativeAt1() / (2*sum(obs)) * ...
-    chi2inv(1-alpha,lpModel.numScenarios - 1);
+    chi2inv(1-alpha,lp.numScenarios - 1);
 
 iiSet = 1:length(rho);
 
 for ii = iiSet
     timeStart = tic;
-    [solvedLRLP,c1,n1] = SolveLRLP( lpModel, phi, obs, rho(ii), 'multi' );
+    [solvedLRLP,c1,n1] = SolveLRLP( lp, phi, obs, rho(ii), 'multi' );
     timeIndiv = toc(timeStart);
     
     p1 = solvedLRLP.pWorst;
