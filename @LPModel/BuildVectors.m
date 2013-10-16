61b0a67cb4414e419ce71911f8f118d09c7ef47d
David Love <dlove@math.arizona.edu>
2013-10-16 12:15:34 -0400

    Refactor to remove much repeated code
    	modified:   @LPModel/BuildVectors.m

--- @LPModel/BuildVectors.m
+++ @LPModel/BuildVectors.m
@@ -7,23 +7,11 @@ function [b1,UB1,LB1,Cost1,b2,UB2,LB2,Cost2,costOut] = BuildVectors(obj, cellInp
 
 numFiles = length(cellInputFile);
 
-for ii=1:numFiles
-    
-    % Unit costs
-    c = xlsread(cellInputFile{ii},'Costs');
-    costVector(:,:,ii) = c(2:end,:);
-    
-    % Variable Bounds
-    u = xlsread(cellInputFile{ii},'Upper Bounds');
-    ub(:,:,ii) = u(2:end,:);
-    l = xlsread(cellInputFile{ii},'Lower Bounds');
-    lb(:,:,ii) = l(2:end,:);
-    
-    % b vector (knowns)
-    R = xlsread(cellInputFile{ii},'b_vec');
-    RHS(:,:,ii) = R(2:end,:);
-    
-end
+% Read input files into three-dimensional arrays
+costVector = ReadInput(obj, cellInputFile, 'Costs');
+lb = ReadInput(obj, cellInputFile, 'Lower Bounds');
+ub = ReadInput(obj, cellInputFile, 'Upper Bounds');
+RHS = ReadInput(obj, cellInputFile, 'b_vec');
 
 f = find(ub==-1);
 ub(f) = Inf(size(f));
@@ -64,68 +52,81 @@ if obj.timeLag ~= 1
     end
 end
 
-% if max(abs([size(cost,2),size(ub,2),size(lb,2),size(RHS,2)]-obj.timePeriods)) > 0
 if obj.timePeriods > min([size(costVector,2),size(ub,2),size(lb,2),size(RHS,2)])
     error('Request for more periods than data provides');
 end
 
+% Remove unnecessary time periods
 costVector = costVector(:,1:obj.timePeriods,:);
 ub = ub(:,1:obj.timePeriods,:);
 lb = lb(:,1:obj.timePeriods,:);
 RHS = RHS(:,1:obj.timePeriods,:);
 
-% B_vec = RHS; % Make copy for post analysis purposes
-
-% Augment the initial storage by the required amounts
-% for ii=1:length(st_nodes)
-%      RHS(strcmp(Nr,st_nodes{ii}),1) = RHS(strcmp(Nr,st_nodes{ii}),1) ...
-%          - st_initial(ii);
-% end
-
-for ii=1:numFiles
-    b1t = RHS(:,1:obj.firstStagePeriods,ii);
-    b1(:,ii) = b1t(:);
-    b2t = RHS(:,obj.firstStagePeriods+1:end,ii);
-    if ii > 1
-        for jj=1:size(b2t,2)
-            b2t(:,jj) = b2t(:,jj) ...
-                - RHS(:,obj.firstStagePeriods,ii) + RHS(:,obj.firstStagePeriods,1);
-        end
-    end
-    b2(:,ii) = b2t(:);
-    
-    
-    UB1t = ub(:,1:obj.firstStagePeriods,ii);
-    UB1(:,ii) = UB1t(:);
-    UB2t = ub(:,obj.firstStagePeriods+1:end,ii);
-    if ii > 1
-        index = ub(:,1,1) < Inf;
-        for jj=1:size(UB2t,2)
-            UB2t(index,jj) = UB2t(index,jj) ...
-                - ub(index,obj.firstStagePeriods,ii) + ub(index,obj.firstStagePeriods,1);
-        end
-    end
-    UB2(:,ii) = UB2t(:);
-    
-    LB1t = lb(:,1:obj.firstStagePeriods,ii);
-    LB1(:,ii) = LB1t(:);
-    LB2t = lb(:,obj.firstStagePeriods+1:end,ii);
-    LB2(:,ii) = LB2t(:);
-    
-    Cost1t = costVector(:,1:obj.firstStagePeriods,ii);
-    Cost1(:,ii) = Cost1t(:);
-    Cost2t = costVector(:,obj.firstStagePeriods+1:end,ii);
-    Cost2(:,ii) = Cost2t(:);
-end
-
-b1 = b1(:,1);
-UB1 = UB1(:,1);
-LB1 = LB1(:,1);
-Cost1 = Cost1(:,1);
-
-% b_vec = RHS(:);
-% UB = ub(:);
-% LB = lb(:);
-% Cost = cost(:);
+% Decompose into two stages
+[b1,b2] = DecomposeStages(obj, RHS, true);
+[UB1, UB2] = DecomposeStages(obj, ub, true);
+[LB1, LB2] = DecomposeStages(obj, lb, false);
+[Cost1, Cost2] = DecomposeStages(obj, costVector, false);
 
 costOut = costVector(:,:,1);
+
+function outMatrix = ReadInput( obj, cellInputFile, sheet )
+% ReadInput reads Inputs.xlsx into three-dimensional arrays
+
+numFiles = length(cellInputFile);
+
+switch sheet
+    case {'Costs', 'Upper Bounds', 'Lower Bounds'}
+        prediction = obj.variableNames;
+    case 'b_vec'
+        prediction = obj.Nr;
+    otherwise
+        error(['Unassigned sheet name ', sheet])
+end
+
+for ii=1:numFiles
+    [inMat,txt] = xlsread(cellInputFile{ii},sheet);
+    if ~isequal( prediction, ...
+            txt(2:2+length(prediction)-1,2) )
+        num = length(prediction);
+        errors = false(1,num);
+        for nn = 1:num
+            errors(nn) = ~isequal( prediction{nn}, txt{1+nn,2} );
+        end
+        errnums = find(errors)';
+        errlist = [{'Excel row', 'Predicted name', 'Excel name'}; ...
+            num2cell(errnums+1), prediction(errors), txt(1+errnums,2)];
+        fprintf('\nERROR PRINTOUT')
+        disp(errlist)
+        error(['Predicted variable names in ''', sheet, ''' do not match those in file', cellInputFile{ii}])
+    end
+    if ii == 1
+        outMatrix = zeros(size(inMat,1)-1, size(inMat,2), numFiles);
+    end
+    outMatrix(:,:,ii) = inMat(2:end,:);
+end
+
+function [stageOne, stageTwo] = DecomposeStages(obj, matrix, adjust)
+% DecomposeStages splits the matrix into vectors for each stage
+
+numFiles = size(matrix,3);
+for ii=1:numFiles
+    stage1Temp = matrix(:,1:obj.firstStagePeriods,ii);
+    if ii == 1
+        stageOne = zeros(numel(stage1Temp), numFiles);
+    end
+    stageOne(:,ii) = stage1Temp(:);
+    stage2Temp = matrix(:,obj.firstStagePeriods+1:end,ii);
+    if ii > 1 && adjust
+        for jj=1:size(stage2Temp,2)
+            stage2Temp(:,jj) = stage2Temp(:,jj) ...
+                - matrix(:,obj.firstStagePeriods,ii) + matrix(:,obj.firstStagePeriods,1);
+        end
+    end
+    if ii == 1
+        stageTwo = zeros(numel(stage2Temp), numFiles);
+    end
+    stageTwo(:,ii) = stage2Temp(:);
+end
+
+stageOne = stageOne(:,1);
