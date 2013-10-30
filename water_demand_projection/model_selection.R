747db7c5626000baf596e8fbb821789f678be194
David Love <dlove@math.arizona.edu>
2013-10-30 11:55:03 -0400

    Add two additional models, update projected demands

--- water_demand_projection/model_selection.R
+++ water_demand_projection/model_selection.R
@@ -4,9 +4,13 @@ grid.type <- 'bcsd5'
 
 model.selection <- c('gfdl.cm3.1.rcp85',
                      'gfdl.cm3.1.rcp45',
+                     'gfdl.esm2m.1.rcp85',
+                     'gfdl.esm2m.1.rcp45',
                      'csiro.mk3.6.0.1.rcp85',
                      'csiro.mk3.6.0.1.rcp45',
                      'hadgem2.es.1.rcp85',
                      'hadgem2.es.1.rcp45',
                      'miroc5.1.rcp85',
-                     'miroc5.1.rcp45')
+                     'miroc5.1.rcp45',
+                     'miroc.esm.chem.1.rcp85',
+                     'miroc.esm.chem.1.rcp45')
