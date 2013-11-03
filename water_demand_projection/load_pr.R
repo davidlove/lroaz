42f9f8f4ab667009fa17fb9bd69cac6256bf70a7
David Love <dlove@math.arizona.edu>
2013-11-03 11:55:49 -0500

    Include data for RCP2.5 and RCP6.0, save as the projected demands
    	new file:   water_demand_projection/monthly_4_emissions/201311022033CXX001_bRXnUa.txt
    	new file:   water_demand_projection/monthly_4_emissions/Notes.txt
    	new file:   water_demand_projection/monthly_4_emissions/Projections5.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/COLS_pr.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/COLS_tasmax.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/Info_pr.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/Info_tasmax.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/MetaData.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/Notes.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/Projections5.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/ROWS_pr.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/ROWS_tasmax.txt
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/pr.csv
    	new file:   water_demand_projection/monthly_4_emissions/bcsd5/tasmax.csv

--- water_demand_projection/load_pr.R
+++ water_demand_projection/load_pr.R
@@ -1,11 +1,12 @@
 # Load the monthly and daily climate date for max temperature
 
 monthly.pr <- list()
-monthly.pr$bc5 <- read.csv('monthly_weather/bc5/pr.csv')
-monthly.pr$bcsd5 <- read.csv('monthly_weather/bcsd5/pr.csv')
-monthly.pr$rgrd5 <- read.csv('monthly_weather/rgrd5/pr.csv')
+#monthly.pr$bc5 <- read.csv('monthly_weather/bc5/pr.csv')
+#monthly.pr$bcsd5 <- read.csv('monthly_weather/bcsd5/pr.csv')
+#monthly.pr$rgrd5 <- read.csv('monthly_weather/rgrd5/pr.csv')
+monthly.pr$bcsd5 <- read.csv('monthly_4_emissions/bcsd5/pr.csv')
 
-daily.pr <- list()
-daily.pr$bc5 <- read.csv('daily_weather/bc5/pr.csv')
-daily.pr$bcca5 <- read.csv('daily_weather/bcca5/pr.csv')
-daily.pr$rgrd5 <- read.csv('daily_weather/rgrd5/pr.csv')
+#daily.pr <- list()
+#daily.pr$bc5 <- read.csv('daily_weather/bc5/pr.csv')
+#daily.pr$bcca5 <- read.csv('daily_weather/bcca5/pr.csv')
+#daily.pr$rgrd5 <- read.csv('daily_weather/rgrd5/pr.csv')
