0625d2046ba398f62cc814ba56d19312a7a42cd8
David Love <dlove@math.arizona.edu>
2013-11-10 13:02:58 -0700

    Add function to find necessary capacity of WWTP and IPR in MGD
    	new file:   MaxInfrastructureUsage.m

---
+++ MaxInfrastructureUsage.m
@@ -0,0 +1,27 @@
+function [wwtpUsage, iprUsage] = MaxInfrastructureUsage( savedDataFile, lp )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+d = load(savedDataFile);
+[scens, rhos] = size(d.scenSolns);
+numVars = size(lp.Abase, 2);
+secondStagePeriods = length(d.scenSolns{1,1}) / numVars;
+wwtpLocs = find(~cellfun(@isempty, regexp(lp.variableNames, '-->> SP_C')));
+iprLocs = find(~cellfun(@isempty, regexp(lp.variableNames, '-->> IPR_C')));
+
+wwtpUsage = zeros(scens, rhos);
+iprUsage = zeros(scens, rhos);
+
+for ii=1:scens
+    for jj=1:rhos
+        solns = d.scenSolns{ii,jj};
+        solns = reshape(solns, numVars, secondStagePeriods);
+        wwtpUsage(ii,jj) = max(sum(solns(wwtpLocs,:), 1));
+        iprUsage(ii,jj) = max(sum(solns(iprLocs,:), 1));
+    end
+end
+
+wwtpUsage = max(max(wwtpUsage)) / 1121; % Convert to MGD
+iprUsage = max(max(iprUsage)) / 1121; % Convert to MGD
+
+end
